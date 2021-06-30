36026392fad766efa9f5ad6be65e0b6977f1ec27
iizma04 <83937139+iizma04@users.noreply.github.com>
2021-06-29 19:45:55 -0700

    Update KEYS.py

--- KEYS.py
+++ KEYS.py
@@ -11,3 +11,28 @@ df = df.drop(df.columns[[7, 8, 9, 10, 11, 12, 13, 14]], axis = 1)
 print(df)
 
 sr = pd.Series([''])
+
+temp = list.files(pattern="*.csv")
+myfiles = lapply(temp, read.delim)
+
+path = r'C:\Users\izmasyed\Desktop\KEYS project\2016 2'
+all_files = glob.glob(path + "/*.csv")
+
+import csv
+import os
+data_files = os.listdir('/Users/izmasyed/Desktop/KEYS project/2016 2')
+def load_files(filenames):
+    for file in filenames:
+        yield pd.read_csv(filename)
+        
+     
+     import pandas as pd
+from glob import glob
+
+stock_files = sorted(glob('arizona-out*.tsv'))
+
+dfs = []
+
+for filename in os.listdir(directory):
+    if filename.endswith(".tsv"):
+        pandas.concat(filename)
